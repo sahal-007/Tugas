f4d41fad7827684f42e59b59efb2b6692f8d3162
sahal <jb007xchange@gmail.com>
2022-12-26 12:31:28 +0700

    fix bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 // import 'package:aplikasi_flutter_sahal/column_widget.dart';
 // import 'package:aplikasi_flutter_sahal/hello_world.dart';
 // import 'package:aplikasi_flutter_sahal/row_widget.dart';
-import 'package:aplikasi_flutter_sahal/ui/produk_form.dart';
+// import 'package:aplikasi_flutter_sahal/ui/produk_form.dart';
+import 'package:aplikasi_flutter_sahal/ui/produk_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: "Aplikasi Flutter Pertama",
-      home: ProdukForm(),
+      home: ProdukPage(),
     );
 }
 }
